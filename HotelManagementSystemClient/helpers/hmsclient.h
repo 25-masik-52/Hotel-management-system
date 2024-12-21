3159e2ca318f9ab48ed146018941ae588ff917ab
25-masik-52 <max2827@yandex.ru>
2024-12-21 03:22:28 +0300

    add_client

--- HotelManagementSystemClient/helpers/hmsclient.h
+++ HotelManagementSystemClient/helpers/hmsclient.h
@@ -1,7 +1,29 @@
 #pragma once
 
-class HmsClient
+#include "hmsobjects.h"
+#include <QObject>
+#include <QList>
+#include <QTcpSocket>
+
+class HmsClient : public QObject
 {
+    Q_OBJECT
+    
 public:
-    HmsClient();
+    HmsClient(QObject *parent = nullptr);
+    
+public:
+    bool connect(const QString &host, quint16 port = 2643);
+    bool addUser(const User &user);
+    bool changeUserData(const User &user);
+    bool deleteUser(const QString &login);
+    User getUserByLogin(const QString &login);
+    bool book(const Order &order);
+    bool removeOrder(int orderId);
+    QList<Order> getOrders();
+    QStringList getOrderedRoomsByDate(const QDate &date);
+    bool rateOrder(int orderId, int rating);
+    
+private:
+    QTcpSocket* m_socket;
 };
--- HotelManagementSystemClient/mainwindow.cpp
+++ HotelManagementSystemClient/mainwindow.cpp
@@ -4,10 +4,34 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow{ parent }
     , m_ui{ new Ui::MainWindow }
+    , m_client{ new HmsClient{} }
+    , m_popup{ new HmsPopupWidget{ this } }
     , m_signInPage{ new SignInMainPage }
     , m_signUpPage{ new SignUpMainPage }
 {
     m_ui->setupUi(this);
+    
+    if (!m_client->connect("https://localhost"))
+    {
+        auto* portLineEdit = new QLineEdit{};
+        portLineEdit->setObjectName("portLineEdit");
+        portLineEdit->setPlaceholderText("Try another port");
+        portLineEdit->setFont(HmsUiHelper::Regular_Font);
+        portLineEdit->setStyleSheet("color: #003049; background: transparent; font-family: 'TT Wellingtons';");
+        
+        m_popup->setParent(m_signInPage);
+        
+        m_popup->setTitle("No connection!")
+            ->addInfoRows({ { "Your application is trying to\nconnect to the server via port\n2643. Check the port with the\nadministrator and try again." } }, true)
+            ->addLineEdit(portLineEdit)
+            ->setButtons(HmsPopupButtons::Refresh);
+        m_popup->getButton(HmsPopupButtons::Refresh)->setText("Try again");
+        
+        connect(m_popup->getButton(HmsPopupButtons::Refresh), &QPushButton::clicked,
+                this, &MainWindow::connectToServer);
+        
+        m_popup->exec();
+    }
     
     m_ui->stackedWidget->addWidget(m_signInPage);
     m_ui->stackedWidget->addWidget(m_signUpPage);
@@ -22,6 +46,12 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete m_ui;
+}
+
+void MainWindow::connectToServer()
+{
+    if (m_client->connect("https://localhost", m_popup->getLineEdit("portLineEdit")->displayText().trimmed().toUInt()))
+        m_popup->close();
 }
 
 void MainWindow::goToAnotherWidget(const QString& className)
